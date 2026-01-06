cad06b1d3385489a1994d1488530825a6594e470
topher3141 <TOPHER3141@GMAIL.COM>
2026-01-06 10:00:36 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
 <body>
+ <script src="/bridge.js" />
+
   <SwRegister />
   {children}
 </body>
